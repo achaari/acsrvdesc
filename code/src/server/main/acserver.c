37ebfffafcabb0bfcb94888df67c8e793ca153f4
achaari <achaari@users.noreply.github.com>
2019-11-01 17:08:59 +0100

    Update acserver.c

--- code/src/server/main/acserver.c
+++ code/src/server/main/acserver.c
@@ -9,16 +9,16 @@ e_srvrunstat_ acsrv_run_server(p_srvhndl_ srvhndlp, int argc, const char **argv)
 	else if (! acsrv_init_server(srvhndp, argc, argv)) {
 		return SRV_RUNSTAT_INIT_FAILED;
 	}
-	
+
 	/* Check Serve Config */
 	if (! acsrv_check_config(srvhndlp)) {
 		return SRV_RUNSTAT_INVALID_CONFIG;
 	}
 	
 	/* Register server */
-  if (! acsrv_register_server(srvhndlp)) {
-    return SRV_RUNSTAT_REG_FAILED;
-  }
+	if (! acsrv_register_server(srvhndlp)) {
+		return SRV_RUNSTAT_REG_FAILED;
+	}
   
-  return SRV_RUNSTAT_OK; 
+  	return SRV_RUNSTAT_OK; 
 }
